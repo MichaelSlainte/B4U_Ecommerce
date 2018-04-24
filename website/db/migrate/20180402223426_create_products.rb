624b8cc8b5eacc0f1aac82c5a5715106ed69406f
Michael Slainte <eng.michaelmarques@outlook.com>
2018-04-24 22:51:08 +0100

    B4U

---
+++ website/db/migrate/20180402223426_create_products.rb
@@ -0,0 +1,14 @@
+class CreateProducts < ActiveRecord::Migration
+  def change
+    create_table :products do |t|
+      t.string :image
+      t.string :name
+      t.string :description
+      t.float :price
+      t.integer :quantity
+      t.string :category
+
+      t.timestamps
+    end
+  end
+end
